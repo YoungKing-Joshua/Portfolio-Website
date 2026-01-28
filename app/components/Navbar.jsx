9b50812c3d409726299ebc7b011005ad735e2f8f
Joshua Dei-Alorse <joshuadeialorse@gmail.com>
2026-01-28 11:37:20 +0000

    updating Navbar Section

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,33 +2,50 @@
 
 import { assets } from "@/public/assets/assets";
 import Image from "next/image";
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 
 const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
   const sideMenuRef = useRef();
 
   const openMenu = () => {
-    sideMenuRef.current.style.transform = "translateX(0rem)";
+    sideMenuRef.current.style.transform = "translateX(-16rem)";
   };
 
   const closeMenu = () => {
     sideMenuRef.current.style.transform = "translateX(16rem)";
   };
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (scroll > 50) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    });
+  }, []);
   return (
     <>
       <div className="fixed top-0 left-0 w-11/12 -z-10 translate-y-[-80%]">
         <Image src={assets.header_bg_color} alt="" className="w-full" />
       </div>
-      <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50">
+      <nav
+        className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex 
+      items-center justify-between z-50"
+      >
         <a href="#top" className="font-Ovo">
           <Image
             alt=""
             src={assets.logo}
-            className="w-28 cursor-pointer mr-14"
+            className="w-28 alt='' cursor-pointer mr-14"
           />
         </a>
 
-        <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3">
+        <ul
+          className="hidden md:flex items-center gap-6 lg:gap-8 
+        rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50"
+        >
           <li>
             <a href="#top" className="font-Ovo">
               Home
@@ -50,7 +67,9 @@ const Navbar = () => {
             </a>
           </li>
           <li>
-            <a href="#contact">Contact Me</a>
+            <a href="#contact" className="font-Ovo">
+              Contact Me
+            </a>
           </li>
         </ul>
 
@@ -60,7 +79,10 @@ const Navbar = () => {
           </button>
           <a
             href="#contact"
-            className="hidden md:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo shadow-sm bg-opacity-50"
+            className="hidden lg:flex 
+            items-center gap-3 px-10 py-2.5 border 
+            border-gray-500 rounded-full 
+            ml-4 font-Ovo"
           >
             Contact
             <Image alt="" src={assets.arrow_icon} className="w-3" />
