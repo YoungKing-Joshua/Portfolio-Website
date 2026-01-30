0eb38e6f74ff3e0b1f410878b194caeb80458618
Joshua Dei-Alorse <joshuadeialorse@gmail.com>
2026-01-30 14:02:45 +0000

    updating Navbar Section

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -17,7 +17,9 @@ const Services = () => {
         {serviceData.map(({ icon, title, description, link }, index) => (
           <div
             key={index}
-            className="border border-gray-400 rounded-lg px-8 py-12 hover:shadow-black cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500"
+            className="border border-gray-400 rounded-lg px-8 py-12 
+            hover:shadow-black cursor-pointer hover:bg-lightHover 
+            hover:-translate-y-1 duration-500"
           >
             <Image src={icon} alt="" className="w-10" />
             <h3 className="text-lg my-4 text-gray-700">{title}</h3>
