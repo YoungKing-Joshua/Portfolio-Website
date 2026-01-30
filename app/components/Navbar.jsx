b51af0c0639f6a8ff2cb062639cf219ba6e364ca
Joshua Dei-Alorse <joshuadeialorse@gmail.com>
2026-01-30 12:06:24 +0000

    updating Navbar Section

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -25,13 +25,14 @@ const Navbar = () => {
   }, []);
   return (
     <>
-      <div className="fixed top-0 left-0 w-11/12 -z-10 translate-y-[-80%]">
+      <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]">
         <Image src={assets.header_bg_color} alt="" className="w-full" />
       </div>
       <nav
-        className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${
-          isScrolled ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm" : ""
-        }`}
+        className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex 
+          items-center justify-between z-50 ${
+            isScrolled ? "bg-white/30 backdrop-blur-lg shadow-sm" : ""
+          }`}
       >
         <a href="#top" className="font-Ovo">
           <Image
@@ -42,8 +43,10 @@ const Navbar = () => {
         </a>
 
         <ul
-          className="hidden md:flex items-center gap-6 lg:gap-8 
-        rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50"
+          className={`hidden md:flex items-center gap-6 lg:gap-8 
+            rounded-full px-12 py-3 ${
+              isScrolled ? "" : "bg-white shadow-sm bg-opacity-50"
+            } bg-white/50 shadow-sm`}
         >
           <li>
             <a href="#top" className="font-Ovo">
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,6 @@
 import { Outfit, Ovo } from "next/font/google";
 
-/** @type {import('tailwindcss').Config} */
+/** @type {import ('tailwindcss').Config} */
 export default {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
@@ -19,9 +19,9 @@ export default {
         Ovo: ["Ovo", "serif"],
       },
       boxShadow: {
-        black: "4px 4px 0 #000",
-        white: "4px 4px 0 #fff",
-      },
+        'black':' 4px 4px 0 #000',
+        'white':' 4px 4px 0 #fff',
+      }
     },
   },
   plugins: [],
