59a74da53604793a4a90d4221e045cc7158f7d50
Joshua Dei-Alorse <joshuadeialorse@gmail.com>
2026-01-30 14:52:41 +0000

    updating Navbar Section

---
+++ app/components/Work.jsx
@@ -0,0 +1,47 @@
+import { assets, workData } from "@/public/assets/assets";
+import Image from "next/image";
+import React from "react";
+
+export default function Work() {
+  return (
+    <div id="work" className="w-full px-[12%] py-10 scroll-mt-20">
+      <h4 className="text-center mb-2 text-lg font-Ovo">My Portfolio</h4>
+      <h2 className="text-center text-5xl font-Ovo">My Latest Work</h2>
+
+      <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">
+        Welcome to my porfolio! Explore a selection of my recent projects that
+        showcase my skills and expertise in software development. Each project
+        reflects my commitment to delivering high-quality solutions and
+        innovative designs.
+      </p>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 my-10 gap-5">
+        {workData.map((project, index) => (
+          <div
+            key={index}
+            className="aspect-square bg-no-repeat bg-cover bg-center rounded-lg
+            relative cursor-pointer group"
+            style={{ backgroundImage: `url(${project.bgImage})` }}
+          >
+            <div
+              className="bg-white w-10/12 rounded-md absolute bottom-5
+            left-1/2 -translate-x-1/2 py-3 px-5 flex items-center 
+            justify-between duration-500 group-hover:-translate-y-1 group-hover:shadow-lg"
+            >
+              <div>
+                <h2 className="font-semibold">{project.title}</h2>
+                <p className="text-sm text-gray-700">{project.description}</p>
+              </div>
+              <div
+                className="border rounded-full w-9
+              aspect-square flex items-center justify-center shadow-[2px_2px_0_#000]
+              group-hover:bg-lime-300 transition"
+              >
+                <Image src={assets.send_icon} alt="send icon" className="w-5" />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
