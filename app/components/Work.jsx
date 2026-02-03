a537a3107e971e7ce831038a174b880a2193814c
Joshua Dei-Alorse <joshuadeialorse@gmail.com>
2026-02-03 10:54:57 +0000

    Updated the Work Section

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -42,6 +42,20 @@ export default function Work() {
           </div>
         ))}
       </div>
+
+      <a
+        href=""
+        className="w-max flex items-center justify-center gap-2
+      text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto 
+      my-20 hover:bg-lightHover duration-500"
+      >
+        Show more
+        <Image
+          src={assets.right_arrow_bold}
+          alt="Right arrow"
+          className="w-4"
+        />
+      </a>
     </div>
   );
 }
