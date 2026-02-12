9a61c5bd9c722243c282e835aade19a73b11c5e2
Joshua Dei-Alorse <joshuadeialorse@gmail.com>
2026-02-12 15:39:18 +0000

    added motion animation to Services Section

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -1,21 +1,53 @@
 import { assets, serviceData } from "@/public/assets/assets";
 import Image from "next/image";
 import React from "react";
+import { motion } from "motion/react";
 
 const Services = () => {
   return (
-    <div id="services" className="w-full px-[12%] py-10 scroll-mt-20">
-      <h4 className="text-center mb-2 text-lg font-Ovo">What I offer</h4>
-      <h2 className="text-center text-5xl font-Ovo">My Services</h2>
+    <motion.div
+      initial={{ pacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+      id="services"
+      className="w-full px-[12%] py-10 scroll-mt-20"
+    >
+      <motion.h4
+        initial={{ y: -20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.3, duration: 0.5 }}
+        className="text-center mb-2 text-lg font-Ovo"
+      >
+        What I offer
+      </motion.h4>
+      <motion.h2
+        initial={{ y: 20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.5, duration: 0.5 }}
+        className="text-center text-5xl font-Ovo"
+      >
+        My Services
+      </motion.h2>
 
-      <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">
+      <motion.p
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ delay: 0.7, duration: 0.5 }}
+        className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo"
+      >
         I specialize in creating responsive web applications with modern
         technologies like React, Next.js, and Node.js, ensuring seamless user
         experiences and robust backend solutions.
-      </p>
-      <div className="grid grid-cols-4 gap-6 my-10">
+      </motion.p>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ delay: 0.9, duration: 0.6 }}
+        className="grid grid-cols-4 gap-6 my-10"
+      >
         {serviceData.map(({ icon, title, description, link }, index) => (
-          <div
+          <motion.div
+            whileHover={{ scale: 1.05 }}
             key={index}
             className="border border-gray-400 rounded-lg px-8 py-12 
             hover:shadow-black cursor-pointer hover:bg-lightHover 
@@ -32,10 +64,10 @@ const Services = () => {
               Read More
               <Image alt="" src={assets.right_arrow} className="w-4" />
             </a>
-          </div>
+          </motion.div>
         ))}
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 };
 
