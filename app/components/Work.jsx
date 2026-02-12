cdf303e7ac016284199398281d4f37aec75c5431
Joshua Dei-Alorse <joshuadeialorse@gmail.com>
2026-02-12 16:08:36 +0000

    added motion animation to Contact Section

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -1,8 +1,7 @@
 import { assets, workData } from "@/public/assets/assets";
 import Image from "next/image";
 import React from "react";
-import { motion, scale } from "motion/react";
-import { delay } from "motion";
+import { motion } from "motion/react";
 
 export default function Work({ DarkMode }) {
   return (
@@ -13,24 +12,24 @@ export default function Work({ DarkMode }) {
       id="work"
       className="w-full px-[12%] py-10 scroll-mt-20"
     >
-      <h4
-        initial={{ opacity: 0 }}
-        whileInView={{ opacity: 1 }}
-        transition={{ duration: 1 }}
+      <motion.h4
+        initial={{ y: -20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.3, duration: 0.5 }}
         className="text-center mb-2 text-lg font-Ovo"
       >
         My Portfolio
-      </h4>
-      <h2
-        initial={{ opacity: 0 }}
-        whileInView={{ opacity: 1 }}
-        transition={{ duration: 1 }}
+      </motion.h4>
+      <motion.h2
+        initial={{ y: -20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.5, duration: 0.5 }}
         className="text-center text-5xl font-Ovo"
       >
         My Latest Work
-      </h2>
+      </motion.h2>
 
-      <p
+      <motion.p
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
         transition={{ delay: 0.7, duration: 0.5 }}
@@ -40,15 +39,15 @@ export default function Work({ DarkMode }) {
         showcase my skills and expertise in software development. Each project
         reflects my commitment to delivering high-quality solutions and
         innovative designs.
-      </p>
-      <div
+      </motion.p>
+      <motion.div
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
         transition={{ delay: 0.9, duration: 0.6 }}
         className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 my-10 gap-5 dark:text-black"
       >
         {workData.map((project, index) => (
-          <div
+          <motion.div
             whileHover={{ scale: 1.05 }}
             transition={{ duration: 0.3 }}
             key={index}
@@ -56,7 +55,7 @@ export default function Work({ DarkMode }) {
             relative cursor-pointer group"
             style={{ backgroundImage: `url(${project.bgImage})` }}
           >
-            <div
+            <motion.div
               className="bg-white w-10/12 rounded-md absolute bottom-5
             left-1/2 -translate-x-1/2 py-3 px-5 flex items-center 
             justify-between duration-500 group-hover:-translate-y-1 group-hover:shadow-lg"
@@ -65,19 +64,19 @@ export default function Work({ DarkMode }) {
                 <h2 className="font-semibold">{project.title}</h2>
                 <p className="text-sm text-gray-700">{project.description}</p>
               </div>
-              <div
+              <motion.div
                 className="border rounded-full w-9
               aspect-square flex items-center justify-center shadow-[2px_2px_0_#000]
               group-hover:bg-lime-300 transition"
               >
                 <Image src={assets.send_icon} alt="send icon" className="w-5" />
-              </div>
-            </div>
-          </div>
+              </motion.div>
+            </motion.div>
+          </motion.div>
         ))}
-      </div>
+      </motion.div>
 
-      <a
+      <motion.a
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
         transition={{ delay: 1.1, duration: 0.5 }}
@@ -94,7 +93,7 @@ export default function Work({ DarkMode }) {
           alt="Right arrow"
           className="w-4"
         />
-      </a>
+      </motion.a>
     </motion.div>
   );
 }
